faf1083711164a5767f5156b3cc1a069a95ce7e2
miachen67 <miachen2005@gmail.com>
2025-04-24 11:59:37 -0700

    1st attempt

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,14 +7,14 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
-        const result = 0;
+        let result = 0;
 
         result = num1 + num2;
 
         return result;
     }
     else {
-        return !add;
+        return add;
     }
 }
 
@@ -25,15 +25,18 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
-    const discounted = []
-    const length = prices.length;
-    let discountedPrice = 0
-    for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
-        discounted.push(discountedPrice);
+    if (prices.length == 0) {
+        return false;
+    } else {
+        let discounted = []
+        const length = prices.length;
+        let discountedPrice = 0
+        for(let i = 0; i < length; i++) {
+            discountedPrice += prices[i] * (1 - discount);
+            discounted.push(discountedPrice);
+        }
+        return discounted;
     }
-
-    return discounted;
 }
 
 // adding a comment to test PR
